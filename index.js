13ee29193a9318cae342008a4a5ebeccf86ab536
smarttest437-hz <smarttest437@gmail.com>
2025-11-28 18:43:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const DB_FILE = "./db.json";
 
 app.use(express.static("."));
